7d2450c3f5d7b774e8a5cef4030dcf245ff055d0
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-03 13:05:00 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos C##/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 10, 20, 30, 40, 50, 60 };
+        Console.WriteLine(+numeros[3]);
+
+        int[] n = new int[] { 3, 4, 5 };
+        Console.WriteLine(+n[1]);
+
+        char[] c = { 'L', 'u', 'i', 's' };
+        Console.WriteLine(c);
+
+        string s = "Mona lisa";
+        Console.WriteLine(s);
+        Console.ReadKey();
+    }
+}
